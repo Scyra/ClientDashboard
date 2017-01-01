5ba481779ffd3720094880015f03e7108e43c7d8
dmanders <delaney@manders.com>
2017-01-01 15:09:31 -0800

    Tweak to include project scss file.

--- src/app.js
+++ src/app.js
@@ -7,20 +7,23 @@ import Split from 'grommet/components/Split';
 import Sidebar from 'grommet/components/Sidebar';
 import Article from 'grommet/components/Article';
 import Section from 'grommet/components/Section';
+import Header from 'grommet/components/Header';
+import Menu from 'grommet/components/Menu';
 
-import '../node_modules/grommet/scss/vanilla/index.scss';
+import './styles/index.scss';
 
 ReactDOM.render(
   <App>
-    <Split>
-      <Sidebar>
-        Navigation.
+    <Split flex="right">
+      <Sidebar colorIndex="neutral-1">
+        <Header><h1>Scyra</h1></Header>
+        <Menu>Menu</Menu>
       </Sidebar>
-      <Article>
-        <Section>
+      <Article basis="full" size="full" flex="grow" full="vertical">
+        <Section alignSelf="stretch" basis="1/2" colorIndex="light-1">
           Section 1
         </Section>
-        <Section>
+        <Section alignSelf="stretch" basis="1//2" colorIndex="light-2">
           Section 2
         </Section>
       </Article>
